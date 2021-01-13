9b37ce7662b5517a990aa93d6e61c57e4bb5329f
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-01-13 13:19:34 -0300

    Add the funcions of adding and removing

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 
-function TodoForm() {
+function TodoForm(props) {
   const [input, setInput] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Previne o padrão, nesse caso ele evita que a pagina de reload toda vez que o input é acionado.
+  const handleChange = (e) => {
+    setInput(e.target.value); // <- Esse aqui é o caminho do input do formulário
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita o padrão, nesse caso ele evita que a pagina de reload toda vez que o input é acionado.
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput(""); // A função desse setInput aqui é limpar o input a cada vez que o botão for acionado.
+  };
+
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
       <input
@@ -14,6 +26,7 @@ function TodoForm() {
         value={input}
         name="text"
         className="todo-input"
+        onChange={handleChange}
       />
       <button className="todo-button">Adicionar tarefa</button>
     </form>
